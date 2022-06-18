ddd1aff742e52a01b8117c17cfadf8a00bde01b0
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-06-18 18:24:19 +0530

    .

--- website/urls.py
+++ website/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('article/', include('article.urls')),
+    path('api/article/', include('article.urls')),
 ]
