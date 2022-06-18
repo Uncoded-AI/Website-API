33f193de8e038dbed359c09d22df985afce2ca5a
Ujjwal Kar <ujjwalkar21@gmail.com>
2022-06-18 17:42:04 +0530

    ..

--- website/settings.py
+++ website/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'article',
-    'rest_framework'
+    'rest_framework',
     'whitenoise.runserver_nostatic',
 ]
 
